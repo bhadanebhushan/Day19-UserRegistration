c57a8ad4f8d9c7f6bdfde6e61fb8e472590719ea
bhadanebhushan <bhadanebhushan97@gmail.com>
2022-04-14 06:31:16 +0530

    Added:Check Regex For Email Address logic

--- src/com/regex/UserRegistrationMain.java
+++ src/com/regex/UserRegistrationMain.java
@@ -12,6 +12,7 @@ public class UserRegistrationMain {
 			System.out.println("Enter 0 : Exit");
 			System.out.println("Enter 1 : To validate First Name ");
 			System.out.println("Enter 2 : To validate Last Name ");
+			System.out.println("Enter 3 : To validate E-mail ");
 
 			switch (scanner.nextInt()) {
 			case 0:
@@ -25,6 +26,9 @@ public class UserRegistrationMain {
 				UserRegistration.validLastName();
 				break;
 
+			case 3:
+				UserRegistration.validEmail();
+				break;
 			}
 		}
 	}
