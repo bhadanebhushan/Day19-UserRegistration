02d093ed5571400770e4b9996124a41f617566ed
bhadanebhushan <bhadanebhushan97@gmail.com>
2022-04-14 06:15:21 +0530

    first commit

---
+++ src/com/regex/UserRegistrationMain.java
@@ -0,0 +1,10 @@
+package com.regex;
+
+public class UserRegistrationMain {
+	public static void welcome() {
+		System.out.println("Welcome to User Registation Program");
+	}
+	public static void main(String[] args) {
+		welcome();
+	}
+}
