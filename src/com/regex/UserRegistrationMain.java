a4c64ea01299188ee46d9c60067a01694f901d11
bhadanebhushan <bhadanebhushan97@gmail.com>
2022-04-14 06:55:20 +0530

    Added:Check Regex For Password Should Have Atleast 1 Special Character logic

--- src/com/regex/UserRegistrationMain.java
+++ src/com/regex/UserRegistrationMain.java
@@ -17,6 +17,8 @@ public class UserRegistrationMain {
 			System.out.println("Enter 5 : To validate Password Rule 1 ");
 			System.out.println("Enter 6 : To validate Password Rule 2 (Upper Case) ");
 			System.out.println("Enter 7 : To validate Password Rule 3 (Upper Case & One Numeric Number)");
+			System.out.println(
+					"Enter 8 : to validate Password Rule 3 (Upper Case & One Numeric Number & One Special Character)");
 
 			switch (scanner.nextInt()) {
 			case 0:
@@ -48,6 +50,10 @@ public class UserRegistrationMain {
 
 			case 7:
 				UserRegistration.validPassRule3();
+				break;
+
+			case 8:
+				UserRegistration.validPassRule4();
 				break;
 
 			default:
